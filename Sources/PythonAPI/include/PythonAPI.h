f68e2b0fa4010f0a5665ec499f34edbe718e35d5
Rockford Wei <rocky@perfect.org>
2018-06-09 16:19:37 -0400

    Tesing on Swift 4.1.2

--- Sources/PythonAPI/include/PythonAPI.h
+++ Sources/PythonAPI/include/PythonAPI.h
@@ -21,6 +21,6 @@
 #ifdef __APPLE__
 #include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Python.framework/Headers/Python.h"
 #else
-#include "/usr/include/python2.7/Python.h"
+#include "/usr/include/python3.6/Python.h"
 #endif
 #endif
